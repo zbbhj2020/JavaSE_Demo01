4321e4f1055cf6414b6da4fc3d1c2e186ee5467c
zbb <342668568@qq.com>
2020-05-04 18:59:34 +0800

    test

--- Java/src/test/java/BlockingQueueDemo02.java
+++ Java/src/test/java/BlockingQueueDemo02.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: zbb
---
+++ Java/src/test/java/GCDemo.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.RandomAccess;
+
+/**
+ * @author: zbb
+ * @create: 2020/5/4 18:53
+ */
+// 工作中请勿使用该代码，用使用请用自己的电脑测试
+// 通过设置JVM参数，该代码运行会出现的各种OOM错误。
+public class GCDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*************GCDemo hello");
+
+        try
+        {
+            String str = "ABCDE";
+            while(true)
+            {
+                str += str + new Random().nextInt(111111) + new Random().nextInt(22222);
+                str.intern();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
